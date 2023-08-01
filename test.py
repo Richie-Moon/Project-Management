9d285cbcecda70b2bef62dac580719a873ea1517
Richie-Moon <richie.moon1green@gmail.com>
2023-08-01 22:20:10 +1200

    figuring out how pyffish works lol

--- test.py
+++ test.py
@@ -1,32 +1,4 @@
-# class Pawn:
-#     def __init__(self, side):
-#         self.side = side
-#         self.file = 'e'
-#         self.rank = 5
-#
-#     def valid_moves(self):
-#         if self.side == 'white':
-#             moves = []
-#             moves.append(self.file + str(self.rank + 1))
-#
-#             return moves
-#
-#
-# class Rook:
-#     def __init__(self, side):
-#         self.side = side
-#         self.file = 'a'
-#         self.rank = 1
-#
-#     def valid_moves(self):
-#         moves = []
-#         for i in range(8):
-#             moves.append(self.file + str(i))  # a1, a2, etc
-#             moves.append(num_to_letter[i] + str(self.rank))  # a1, b1, c1, etc
-#
-#         return moves
-#
-#
-# num_to_letter = {0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', 6: 'g',
-#                  7: 'h'}
+import pyffish
+
+print(pyffish.legal_moves("", "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", []))
 
