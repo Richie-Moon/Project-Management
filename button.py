e945159a34ec7de37c8e30075b735ec08bb809c9
Richie Moon <richie.moon1green@gmail.com>
2023-08-24 12:37:22 +1200

    Started Main menu GUI.

--- button.py
+++ button.py
@@ -1,39 +1,54 @@
 import pygame
+BORDER: int = 20
 
 
 class Button:
-    def __init__(self, image: pygame.Surface | None, pos: tuple[int],
-                 text_input: str, font: pygame.font.Font, base_colour: str,
-                 hover_colour: str):
-        self.image = image
+    def __init__(self, pos: tuple[int, int], text_input: str,
+                 font: pygame.font.Font,
+                 base_colour: tuple[int, int, int],
+                 hover_colour: tuple[int, int, int],
+                 bg_base_colour: tuple[int, int, int],
+                 bg_hover_colour: tuple[int, int, int],
+                 transparent: bool) -> None:
         self.x_pos = pos[0]
         self.y_pos = pos[1]
         self.font = font
         self.base_colour, self.hovering_colour = base_colour, hover_colour
         self.text_input = text_input
-        self.text = self.font.render(text=self.text_input, antialias=True,
-                                     color=self.base_colour)
+        self.text = self.font.render(self.text_input, True, self.base_colour)
 
-        if self.image is None:
-            self.image = self.text
-        self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+        self.bg_base_colour = bg_base_colour
+        self.bg_hover_colour = bg_hover_colour
+
         self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
+        self.rect = self.text_rect.copy()
 
-    def update(self, screen: pygame.Surface):
-        if self.image is not None:
-            screen.blit(self.image, self.rect)
+        w, h = self.text_rect.size
+        self.rect.update(self.text_rect.left - BORDER, self.text_rect.top,
+                         w + (BORDER * 2), h - (BORDER * 2))
+
+        self.OPACITY = 255//2
+
+        self.bg = pygame.Surface(self.rect.size, pygame.SRCALPHA)
+        if transparent:
+            self.bg.set_alpha(self.OPACITY)
+
+    def update(self, screen: pygame.Surface) -> None:
+        screen.blit(self.bg, self.rect)
         screen.blit(self.text, self.text_rect)
 
-    def check_position(self, position: tuple[int, int]):
+    def check_position(self, position: tuple[int, int]) -> bool:
         if (position[0] in range(self.rect.left, self.rect.right) and
                 position[1] in range(self.rect.top, self.rect.bottom)):
             return True
         return False
 
-    def change_colour(self, position: tuple[int, int]):
+    def change_colour(self, position: tuple[int, int]) -> None:
         if self.check_position(position):
-            self.text = self.font.render(text=self.text_input, antialias=True,
-                                         color=self.hovering_colour)
+            self.text = self.font.render(self.text_input, True,
+                                         self.hovering_colour)
+            self.bg.fill(self.bg_hover_colour)
         else:
-            self.text = self.font.render(text=self.text_input, antialias=True,
-                                         color=self.base_colour)
+            self.text = self.font.render(self.text_input, True,
+                                         self.base_colour)
+            self.bg.fill(self.bg_base_colour)
--- gui.py
+++ gui.py
@@ -4,6 +4,20 @@ import ctypes
 from button import Button
 from typing import Literal
 
+# Constants
+BLACK = (0, 0, 0)
+
+TITLE_SIZE = 150
+HALF = 2
+TITLE_POSITION = 8
+
+BUTTON_TEXT_SIZE = 120
+TEXT_BASE_COLOUR = (255, 255, 255)
+TEXT_HOVER_COLOUR = (0, 0, 0)
+
+BUTTON_BASE_COLOUR = (0, 0, 0)
+BUTTON_HOVER_COLOUR = (255, 255, 255)
+
 pygame.init()
 
 # https://stackoverflow.com/questions/68831902/how-to-make-a-pygame-window-
@@ -21,11 +35,11 @@ w = info.current_w
 bg = pygame.image.load("assets/bg.jpg")
 
 
-def render_font(size: int,
-                style: Literal["Bold", "Medium", "Regular", "SemiBold"] =
-                "Regular") -> pygame.font.Font:
-    style.capitalize()
-    return pygame.font.Font(name=f"assets/fonts/Ruwudu-{style}.tff", size=size)
+def get_font(size: int,
+             style: Literal["Bold", "Medium", "Regular", "SemiBold"] =
+             "Regular") -> pygame.font.Font:
+    style = style.capitalize()
+    return pygame.font.Font(f"assets/fonts/Ruwudu-{style}.ttf", size)
 
 
 # https://stackoverflow.com/questions/20002242/how-to-scale-images-to-
@@ -33,21 +47,81 @@ def render_font(size: int,
 def scale_image(image, size):
     iwidth, iheight = image.get_size()
     scale = max(size[0] / iwidth, size[1] / iheight)
-    new_size = (round(iwidth * scale), round(iheight * scale))
+    new_size = round(iwidth * scale), round(iheight * scale)
     scaled_image = pygame.transform.smoothscale(image, new_size)
     image_rect = scaled_image.get_rect(center=(size[0] // 2, size[1] // 2))
     return scaled_image, image_rect
 
 
+def display_title():
+    title_text_1 = get_font(TITLE_SIZE, "Bold").render("MINI", True,
+                                                       "white")
+    title_text_2 = get_font(TITLE_SIZE, "Bold").render(
+        "CHESS", True, "white")
+
+    title_1_rect = title_text_1.get_rect(center=(w / HALF, h /
+                                                 TITLE_POSITION))
+    title_2_rect = title_text_2.get_rect(center=(w / HALF,
+                                                 h / TITLE_POSITION +
+                                                 TITLE_SIZE))
+    screen.blit(title_text_1, title_1_rect)
+    screen.blit(title_text_2, title_2_rect)
+
+
 def main_menu():
+    screen.fill(BLACK)
     pygame.display.set_caption("Mini Chess")
+
+    # Set scaled background image.
+    bg_scaled, rect = scale_image(bg, (w, h))
+    # Play button
+    play_button = Button(pos=(w // HALF, h // HALF), text_input="PLAY",
+                         font=get_font(BUTTON_TEXT_SIZE),
+                         base_colour=TEXT_BASE_COLOUR,
+                         hover_colour=TEXT_HOVER_COLOUR,
+                         bg_base_colour=BUTTON_BASE_COLOUR,
+                         bg_hover_colour=BUTTON_HOVER_COLOUR,
+                         transparent=True)
+
+    pygame.display.update()
 
     while True:
         mouse_pos = pygame.mouse.get_pos()
+        screen.blit(bg_scaled, rect)
 
-        # Set background image.
-        image, rect = scale_image(bg, (w, h))
-        screen.blit(image, rect)
+        display_title()
+
+        play_button.change_colour(mouse_pos)
+        play_button.update(screen)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            elif (event.type == pygame.MOUSEBUTTONUP and
+                  play_button.check_position(mouse_pos) is True):
+                play()
+
+        pygame.display.update()
+
+
+def settings():
+    while True:
+        moouse_pos = pygame.mouse.get_pos()
+        screen.fill(BLACK)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+
+def play():
+    while True:
+        mouse_pos = pygame.mouse.get_pos()
+        screen.fill(BLACK)
+
+        play_text = get_font(50).render("This is the PLAY screen. ",
+                                        True, "white")
+        screen.blit(play_text, play_text.get_rect(center=(w//2, h//2)))
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
